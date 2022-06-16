8ad88def28ef10faa7384ceb52568bd7720a3c48
saimukzi <saimukzi@proton.me>
2022-06-16 22:23:19 +0800

    full 1080p screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 // https://pixijs.io/guides/basics/getting-started.html
 import * as PIXI from 'pixi.js';
 
+document.body.style.overflow = "hidden";
+document.body.style.margin = "0px";
+
 // Create the application helper and add its render target to the page
-let app = new PIXI.Application({ width: 640, height: 360 });
+let app = new PIXI.Application({ width: 1920, height: 1080 });
 document.body.appendChild(app.view);
 
 // Create the sprite and add it to the stage
