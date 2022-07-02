6db4811c2f7d960daa1de934c317a85edf1a90f7
saimukzi <saimukzi@proton.me>
2022-07-02 19:02:31 +0800

    new class StateMgr

---
+++ src/js/minesweeper/state_mgr.js
@@ -0,0 +1,20 @@
+// import StateMgr from '/js/minesweeper/state_mgr.js';
+
+'use strict';
+
+export const StateMgr = (function(){
+
+class StateMgr {
+
+  constructor(parent){
+    const self = this;
+    self.parent = parent;
+  };
+
+};
+
+return StateMgr;
+
+})();
+
+export default StateMgr;
