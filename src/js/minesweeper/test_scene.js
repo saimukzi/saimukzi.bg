1ebb5115e8ef31fa82c803ad67662852ec624084
saimukzi <saimukzi@proton.me>
2022-07-04 13:51:58 +0800

    add test btn and test move func

--- src/js/minesweeper/test_scene.js
+++ src/js/minesweeper/test_scene.js
@@ -3,6 +3,7 @@
 import * as smz_game_object from '/js/smz/smz_game_object.js';
 import * as PIXI from 'pixi.js';
 import MainScene from '/js/minesweeper/main_scene.js';
+import SmzCommon from '/js/smz/smz_common.js';
 
 import CellGroupGameObject from './cell_group_gobj.js';
 import CellGameObject from './cell_gobj.js';
@@ -24,6 +25,29 @@ class TestScene extends smz_game_object.SmzGameObject {
     
     self.testCellGObj = new CellGameObject(self.runtime);
     self.addChild(self.testCellGObj);
+    
+    self.testBtn = new PIXI.Graphics();
+    self.testBtn.beginFill(0x7fffff);
+    self.testBtn.drawRect(0,0,100,100);
+    self.testBtn.endFill();
+    self.testBtn.interactive = true;
+    self.testBtn.hitArea = new PIXI.Rectangle(0,0,100,100);
+    self.testBtn.mouseup = (ev)=>{self.onTestBtnClick();};
+    self.addChild(self.testBtn);
+    self.testBtn.position.x = 0;
+    self.testBtn.position.y = 100;
+    
+  };
+
+  onTestBtnClick(){
+    const self = this;
+    Promise.resolve()
+      .then(()=>{console.log("onMouseUp Start");})
+      //.then(SmzCommon.waitPromiseFactory(1000))
+      .then(SmzCommon.displayObjLinearMoveToPosPromiseFactory(
+        self.testCellGObj.block,self.runtime.app.ticker,1,1,250
+      ))
+      .then(()=>{console.log("onMouseUp End");});
   };
 
 };
