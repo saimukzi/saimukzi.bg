f551df325971c8d02d5a7e654996bcc35b5264da
saimukzi <saimukzi@proton.me>
2022-07-04 11:43:26 +0800

    CellGameObject add maskContainer

--- src/js/minesweeper/cell_gobj.js
+++ src/js/minesweeper/cell_gobj.js
@@ -25,26 +25,29 @@ class CellGameObject extends smz_game_object.SmzGameObject {
     self.scaleContainer.position.x = 50;
     self.scaleContainer.position.y = 50;
     self.addChild(self.scaleContainer);
-    
+
     self.nBlock = new PIXI.Graphics();
     self.nBlock.beginFill(MwCommon.COLOR_N1);
     self.nBlock.drawRect(-0.5,-0.5,1,1);
     self.nBlock.endFill();
     self.scaleContainer.addChild(self.nBlock);
-    
+
+    self.maskContainer = new PIXI.Container();
+    self.scaleContainer.addChild(self.maskContainer);
+
     self.block = new PIXI.Graphics();
     self.block.beginFill(MwCommon.COLOR_P1);
     self.block.drawRect(-SQRT2/2,-SQRT2/2,SQRT2,SQRT2);
     self.block.endFill();
     self.block.angle = 45;
-    self.scaleContainer.addChild(self.block);
+    self.maskContainer.addChild(self.block);
 
     var mask = new PIXI.Graphics();
     mask.beginFill(0xffffff);
     mask.drawRect(-0.5,-0.5,1,1);
     mask.endFill();
-    self.scaleContainer.mask = mask;
-    self.scaleContainer.addChild(mask);
+    self.maskContainer.mask = mask;
+    self.maskContainer.addChild(mask);
   };
 
 };
