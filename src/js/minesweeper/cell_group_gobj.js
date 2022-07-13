11f32b78b8aab13eedbe8b02a1c098ee7cf70c0c
saimukzi <saimukzi@proton.me>
2022-07-13 22:17:59 +0800

    change debug obj name

--- src/js/minesweeper/cell_group_gobj.js
+++ src/js/minesweeper/cell_group_gobj.js
@@ -63,8 +63,8 @@ class CellGroupGameObject extends smz_game_object.SmzGameObject {
 
   createDebugObj(){
     const self = this;
-    self.debugObj = MwMedia.createRoundBoxGraphics(0xff0000,30);
-    self.addChild(self.debugObj);
+    self.centerDebugObj = MwMedia.createRoundBoxGraphics(0xff0000,30);
+    self.addChild(self.centerDebugObj);
   };
 
 };
