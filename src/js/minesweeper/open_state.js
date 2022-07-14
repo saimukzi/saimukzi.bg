1c795b95c14c98fb29e8c8aa6ed385b46a740a78
saimukzi <saimukzi@proton.me>
2022-07-14 21:44:14 +0800

    part of code

--- src/js/minesweeper/open_state.js
+++ src/js/minesweeper/open_state.js
@@ -9,13 +9,25 @@ export const OpenState = (function(){
 
 const C = SmzCommon;
 
+const BIG_FLIP_MS = 1000;
+const SMALL_FLIP_MS = 250;
+const SHOW_MS = 500;
+const TOTAL_MS = BIG_FLIP_MS+SMALL_FLIP_MS+SHOW_MS+BIG_FLIP_MS+SMALL_FLIP_MS;
+
 const OpenState = {};
 
 OpenState.goAsync = async function(parentMainScene, tData){
   var myTData = tData;
+  var flipCenter;
+  var rad;
 
+  self.parentMainScene.dummyContainerTransform(tData.planMs+(TOTAL_MS/2));
+  flipCenter = new PIXI.Point(960,540);
+  self.parentMainScene.dummyContainer.toLocal(flipCenter,self.parentMainScene,flipCenter);
+  self.parentMainScene.cellGameGObj.debugStartObj.position.copyFrom(flipCenter);
+
+  rad = ;
   
-  myTData = await C.waitPromise(myTData,2000);
 
   return myTData;
 };
